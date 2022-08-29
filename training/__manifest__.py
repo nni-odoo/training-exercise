b045c8c9284b3c029e2af5df7c4f15dc44b8800c
nni-odoo <nni@odoo.com>
2022-08-29 10:42:09 +0800

    [ADD} Day 9 source codes

---
+++ training/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Training',
+    'version': '1.1',
+    'description': "Training",
+    'website': 'https://www.odoo.com/',
+    'depends': [
+        "base",
+        "sale"
+    ],
+    'data': [
+    ],
+    'license': 'LGPL-3',
+}
---
+++ training/models/sale_order.py
@@ -0,0 +1,25 @@
+from odoo import api, fields, models
+
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+
+    @api.model
+    def get_amount_per_partner_search(self, customers):
+        # use search to get the amount per partner
+        data = {}
+        for customer in customers:
+            sale_orders = self.search([('partner_id', '=', customer.id)])
+            data[customer.id] = sum(sale_orders.mapped('amount_total'))
+        return data
+
+    @api.model
+    def get_amount_per_partner_read_group(self, customers):
+        # using read_group to get the amount per partner
+        data = {}
+        sale_order_groups = self.read_group([('partner_id', 'in', customers.ids)], ["amount_total"], ["partner_id"])
+        for group in sale_order_groups:
+            data[group['partner_id'][0]] = group['amount_total']
+        return data
+
+    def get_highest_sale(self):
+        return self.env['sale.order'].search([], order="amount_total DESC", limit=1)
