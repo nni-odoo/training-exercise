7ad7a14fba7de42c2c0d4fa5094c0a6b7967d33b
nni-odoo <nni@odoo.com>
2022-09-01 10:31:47 +0800

    [ADD] Day 7

---
+++ training/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+
+{
+    'name': 'Training',
+    'version': '1.0',
+    'summary': 'Training',
+    'description': """
+        Training
+    """,
+    'depends': ['base', 'sale'],
+    'data': [
+        "security/res_groups.xml",
+        "security/ir.model.access.csv",
+        "security/ir_rule.xml",
+    ],
+    'license': 'LGPL-3',
+}
