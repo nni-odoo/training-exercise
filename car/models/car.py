d1350db0b057245a5ef0923393563e021aec2713
nni-odoo <nni@odoo.com>
2024-01-03 13:26:47 +0700

    additional on returning action

--- car/models/car.py
+++ car/models/car.py
@@ -14,3 +14,30 @@ class Car(models.Model):
 
     def add_miles(self):
         self.miles += 1000
+
+    def show_cars(self):
+        action = {
+            "type": "ir.actions.act_window",
+            "res_model": "car.car",
+            "view_mode": "tree,form"
+        }
+        return action
+
+    def show_another(self):
+        another_car = self.search([('id', '!=', self.id)], limit=1)
+        action = {
+            "type": "ir.actions.act_window",
+            "res_model": "car.car",
+            "view_mode": "form",
+            "res_id": another_car.id,
+        }
+        return action
+
+    def show_others(self):
+        action = {
+            "type": "ir.actions.act_window",
+            "res_model": "car.car",
+            "view_mode": "tree,form",
+            "domain": [('id', '!=', self.id)],
+        }
+        return action
